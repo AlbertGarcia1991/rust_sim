e736db43433f572602847b5186a53ae16fabf6e0
Albert_Garcia_Plaza <albert@quell.tech>
2023-10-05 17:39:33 +0100

    Genome and Gene docstrincs added

--- src/genome.rs
+++ src/genome.rs
@@ -4,57 +4,79 @@ use std::mem;
 use std::cmp;
 use std::sync::atomic;
 
-static COUNTER: atomic::AtomicUsize = atomic::AtomicUsize::new(0);
+static GENE_ID: atomic::AtomicUsize = atomic::AtomicUsize::new(0);
 
+/// Increases by one the value of the GENE_ID counter
 fn bump_counter() {
-    // Add one using the most conservative ordering.
-    COUNTER.fetch_add(1, atomic::Ordering::SeqCst);
+    GENE_ID.fetch_add(0, atomic::Ordering::SeqCst);
 }
 
+/// Returns the current value of the GENE_ID counter
+pub fn get_counter() -> usize {
+    GENE_ID.load(atomic::Ordering::SeqCst)
+}
+
+/// Increases by one the value of the GENE_ID counter and returns this new value
 fn draw_counter() -> usize {
-    let curr_counter: usize = COUNTER.load(atomic::Ordering::SeqCst);
     bump_counter();
+    let curr_counter: usize = GENE_ID.load(atomic::Ordering::SeqCst);
     curr_counter
 }
 
-pub fn get_counter() -> usize {
-    COUNTER.load(atomic::Ordering::SeqCst)
-}
 
-// Main structures
+/// We define a Gene as a structure that contains 4 bytes (source neuron id, neuron weight, neuron 
+/// bias, and destination sink id), plus another u32 bit value which is the bit-wise concatenation 
+/// of the previous four values, being source the MSB and sink the LSH. Hence, two Genes are unique 
+/// if and only if they hold the same value. This is also very useful to understand at each 
+/// iteration the ADN diversity of the entities
 struct Gene {
+    /// Source neuron id
     source: u8,
+    /// Source neuron weight
     weight: u8,
+    /// Source neuron bias
     bias: u8,
+    /// Sink (destination) neuron wid
     sink: u8,
-    value: u32,
+    /// Unique value of the current configuration of bytes
+    value: u32
 }
 
+/// We define the Genome as a structure that contains an unique identifier and a the adn as a vector 
+/// of Genes. This vector has a fixed length of GENOME_SIZE genes inside. The identifier is unique 
+/// for each Genome, hence, two different instances of Genome will have a different id even if they 
+/// have the same adn inside.
 struct Genome {
+    /// Unique identifier of the Genome
     id: u32,
+    /// Vector of Gene objects
     adn: Vec<Gene>
 }
 
-// Traits overloading
 impl std::fmt::Display for Gene {
+    /// A trait that overloads the print!() macro of a Gene by showing the fours bits is made of 
+    /// separated by a point.
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}.{}.{}.{}", self.source, self.weight, self.bias, self.sink)
     }
 }
 
 impl cmp::PartialEq for Gene {
+    /// A trait that overloads the equal comparison between two Genes
     fn eq(&self, other: &Self) -> bool {
         self.value == other.value
     } 
     
+    /// A trait that overloads the not equal comparison between two Genes
     fn ne(&self, other: &Self) -> bool {
         self.value != other.value
     } 
 }
 
-// Custom traits
+
 impl Gene {
-    // TODO: Docstring
+    /// Constructor of a Gene by passing an array with the four bytes of the object. The unique 
+    /// value for that combination of genes is computed automatically.
     pub fn new_from_bytes(bytes: [u8; 4]) -> Self {
         unsafe {
             Gene {
@@ -67,8 +89,10 @@ impl Gene {
         }
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
+    /// Constructor of a Gene to create it randomly. The unique value for that combination of genes 
+    /// is computed automatically. It is unsafe since we are transmuting those four bytes to 
+    /// generate the unique value.
     pub fn new_random() -> Self {
         let max_value_u8: u16 = u8::MAX as u16 + 1;
         unsafe {
@@ -84,9 +108,10 @@ impl Gene {
         }
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
-    pub fn rebuild(&mut self) {
+    /// Trait to assign a value to each one of the four bytes of the Gene based on the Gene's
+    /// unique value.
+    fn rebuild(&mut self) {
         unsafe {
             let bytes: [u8; 4] = mem::transmute(self.value);
             self.source = bytes[0];
@@ -96,15 +121,16 @@ impl Gene {
         }
     }
     
-    // TODO: Docstring
     // TODO: UnitTest
+    //// Trait to return the four bytes of a Gene as an array.
     fn to_bytes(&self) -> [u8; 4] {
         let bytes: [u8; 4] = [self.source, self.weight, self.bias, self.sink];
         bytes
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
+    /// Trait to perform a random mutation on a Gene. We understand as mutation the flip of a single 
+    /// bit only in one of the 4 bytes of the Gene. 
     fn mutate_random(&mut self) {
         let draw_random = rand::thread_rng().gen_range(0..GENOME_MUTATION_TRIES);
         if draw_random < GENOME_MUTATION_RATE {
@@ -114,38 +140,38 @@ impl Gene {
         }
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
-    fn mutate_on_rate(&mut self, rate: u16) {
+    /// Trait to perform a mutation on a Gene based on the given odds. Odds is the probability of a 
+    /// mutation as a part per mil (e.g. if odds=10, there is 1% of chances to mutate: 10 / 1000). 
+    /// We understand as mutation the flip of a single bit only in one of the 4 bytes of the Gene.
+    fn mutate_on_odds(&mut self, odds: u16) {
         let draw_random = rand::thread_rng().gen_range(0..GENOME_MUTATION_TRIES);
-        if draw_random < rate {
+        if draw_random < odds {
             let mutation_mask: u32 = 1;
             self.value ^= mutation_mask << rand::thread_rng().gen_range(0..32);
             self.rebuild();
         }
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
+    /// Trait to always mutate the Gene. We understand as mutation the flip of a single bit only in 
+    /// one of the 4 bytes of the Gene.
     fn mutate_deterministic(&mut self) {
         let mutation_mask: u32 = 1;
         self.value ^= mutation_mask << rand::thread_rng().gen_range(0..32);
         self.rebuild();
     }
 
+    /// Trait to print the the whole binary number of the Gene (unique value).
     fn print_binary(&self) {
-        println!("{:0<8b}{:0<8b}{:0<8b}{:0<8b}", self.source, self.weight, self.bias, self.sink);
-    }
-
-    fn print_bytes(&self) {
-        println!("{}.{}.{}.{}", self.source, self.weight, self.bias, self.sink);
+        println!("{:0<32b}", self.value);
     }
 
 }
 
 impl Genome {
-    // TODO: Docstring
     // TODO: UnitTest
+    /// Constructor to create Genome object with a random adn sequence.
     pub fn new_random() -> Self {
         let genome_id: u32 = draw_counter() as u32;
         let mut adn: Vec<Gene> = Vec::new();
@@ -156,27 +182,37 @@ impl Genome {
         Genome {id: genome_id, adn: adn}
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
+    /// Trait to perform a random mutation on each Gene. 
     pub fn mutate_random(&mut self) {
         for gene in self.adn.iter_mut() {
             gene.mutate_random();
         }
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
+    /// Trait to perform a certain mutation on each Gene. 
     pub fn mutate_deterministic(&mut self) {
         for gene in self.adn.iter_mut() {
             gene.mutate_deterministic();
         }
     }
 
-    // TODO: Docstring
     // TODO: UnitTest
-    pub fn mutate_on_rate(&mut self, rate: u16) {
+    /// Trait to perform a mutation on each Gene based on the given odds. 
+    pub fn mutate_on_odds(&mut self, odds: u16) {
         for gene in self.adn.iter_mut() {
-            gene.mutate_on_rate(rate);
+            gene.mutate_on_odds(odds);
+        }
+    }
+
+    /// Trait to print the whole Genome sequence.
+    pub fn print(&self) {
+        println!("Genoma ID: {}", self.id);
+        let mut gene_counter: u8 = 1;
+        for gene in self.adn.iter() {
+            println!("Gen {gene_counter}:\t{}", gene.value)
+            gene_counter += 1;
         }
     }
 }
